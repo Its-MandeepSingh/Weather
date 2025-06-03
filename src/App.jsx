ae564c1fb76d183844b814699f7ecf550b2f6bfd
Its-MandeepSingh <mandeepsinghkumawat15@gmail.com>
2025-06-03 09:01:23 +0530

    wheather

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Weather from './component/Weather'
+
+
+const App = () => {
+  return (
+    <div className="w-full flex flex-nowrap items-center justify-center  h-[100vh] bg-[url('/ChatGPT%20Image%20Jun%202,%202025,%2005_00_45%20PM.png')] bg-cover bg- bg-repeat-round bg-fill">     
+      <Weather/>
+    </div>
+  )
+}
+
+export default App
